2b2345c13df0b77f9d251131296f5e463278a10f
Arbey <132629931+ArbeyMaya@users.noreply.github.com>
2024-10-24 10:32:38 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w+$@3v*a=ytk-jg-6$lxjnva8q+@$v05_)go)_-#b2ybokilzq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.14.127.25','localhost']
 
 
 # Application definition
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
